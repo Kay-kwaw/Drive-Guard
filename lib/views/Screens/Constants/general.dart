6dbf55f8f75c64780476e16f5a41592af016cd23
kwaw kumi <kwawkumi9@gmail.com>
2023-12-25 06:56:15 +0000

    fonts

--- lib/views/Screens/Constants/general.dart
+++ lib/views/Screens/Constants/general.dart
@@ -1,12 +1,24 @@
 
 import 'package:flutter/material.dart';
+const Color kBackgroundColor =Colors.white;
 
-// Define the background color constant
-const Color kBackgroundColor = Color(0xFFEFEFEF);
 
-// Define the app theme constants
 final ThemeData kAppTheme = ThemeData(
   primaryColor:Colors.white,
-  hintColor: Colors.black, // Replace with your desired font
-  // Add more theme configurations as needed
+  hintColor: Colors.black,
+  secondaryHeaderColor: Colors.black,
+  textTheme: TextTheme(
+    displayLarge: const TextStyle(
+      color: Colors.black,
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      fontFamily: 'Montserrat',
+    ),
+    headline2: const TextStyle(
+      color: Colors.black,
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
+      fontFamily: "Montserrat",
+    ),
+  ),
 );
