70c68a4100f34f661aeabdc422ce88a1431aaeb6
kwaw kumi <kwawkumi9@gmail.com>
2023-12-25 07:30:00 +0000

    app_constants

--- lib/views/Screens/Constants/app_constants.dart
+++ lib/views/Screens/Constants/app_constants.dart
@@ -7,38 +7,19 @@ final ThemeData kAppTheme = ThemeData(
   primaryColor:Colors.white,
   hintColor: Colors.black,
   secondaryHeaderColor: Colors.black,
-  textTheme: TextTheme(
-    displayLarge: const TextStyle(
-      color: Colors.black,
-      fontSize: 20,
-      fontWeight: FontWeight.bold,
-      fontFamily: 'Montserrat',
-    ),
-    headline2: const TextStyle(
-      color: Colors.black,
-      fontSize: 18,
-      fontWeight: FontWeight.bold,
-      fontFamily: "Montserrat",
-    ),
-  ),
-);
+  );
 
 class AppFonts {
+  // ignore: constant_identifier_names
   static const String Montserrat = 'Montserrat';
-  // Add more font constants as needed
-
-  // TextStyle constants for common text styles
   static const TextStyle headline1 = TextStyle(
     fontFamily: Montserrat,
     fontSize: 28.0,
     fontWeight: FontWeight.bold,
   );
-
   static const TextStyle bodyText = TextStyle(
     fontFamily: Montserrat,
     fontSize: 16.0,
     fontWeight: FontWeight.normal,
   );
-
-  // Add more TextStyle constants as needed
 }
