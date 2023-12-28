fb24cafa0c17d752eeb8b471d57071b2067cc242
kwaw kumi <kwawkumi9@gmail.com>
2023-12-28 20:48:16 +0000

    button

--- lib/views/Screens/Addcar/addcar.dart
+++ lib/views/Screens/Addcar/addcar.dart
@@ -12,13 +12,30 @@ class _AddcarWdigetState extends State<AddcarWdiget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: kBackgroundColor,
       appBar: AppBar(
+        title: const Text("Add a car", style: TextStyle(color: kPrimaryColor, fontSize: 18),),
+        centerTitle: false,
         leading: const Icon(Icons.arrow_back, color: kPrimaryColor,),
         backgroundColor: kBackgroundColor,
       ),
       body: SafeArea(
         top: true,
-        child: ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset("assets/images/car_image.png"),
+            ),
+             Padding(
+             padding: const EdgeInsets.all(8.0),
+             child: ElevatedButton(
+              onPressed: (){},
+              child: const Text("Upload a Photo", style: TextStyle(color: Colors.black),)),
+              )
+          ],
+        )
+        ),
 
     );
   }
