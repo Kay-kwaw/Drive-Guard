c3512e89546c813ec3bb479e511fb7e67a34197f
kwaw kumi <kwawkumi9@gmail.com>
2024-01-17 03:35:13 +0000

    welcome_screen

---
+++ lib/views/Screens/Onboarding/welcome_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class _WelcomePagState extends StatefulWidget {
+  const _WelcomePagState({super.key});
+
+  @override
+  State<_WelcomePagState> createState() => __WelcomePagStateState();
+}
+
+class __WelcomePagStateState extends State<_WelcomePagState> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
